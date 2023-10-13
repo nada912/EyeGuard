720284bed3e74e762cbfa533821b0bd3ca71fe71
Nada NADIRE <nadita8225@gmail.com>
2023-10-13 15:54:19 +0200

    new file:   src/components/Footer.vue 	new file:   src/components/Header.vue 	new file:   src/components/Navbar.vue 	new file:   src/components/Slide.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>&copy; 2023 EyeGuard. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="list-inline text-md-right">
+                        <li class="list-inline-item">
+                            <router-link to="/about-us">About Us</router-link>
+                        </li>
+                        <li class="list-inline-item">
+                            <router-link to="/confidentiality">Privacy Policy</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+footer {
+    background-color: #f8f9fa;
+    padding: 20px 0;
+    margin-top: 50px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="path/to/logo.png" alt="Logo">
+            <h1>{{ title }}</h1>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            title: 'EyeGuard'
+        }
+    }
+}
+</script>
+
+<style>
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1rem;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+}
+
+.logo img {
+    height: 50px;
+    margin-right: 1rem;
+}
+
+nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+nav li {
+    margin-right: 1rem;
+}
+
+nav a {
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+}
+</style>
