bcf65e8b55ced7a333356ba1c5debcb67758f95e
Nada NADIRE <nadita8225@gmail.com>
2023-10-22 17:18:07 +0200

    modified:   src/views/Confidentiality.vue

--- src/views/Confidentiality.vue
+++ src/views/Confidentiality.vue
@@ -1,19 +1,116 @@
-/**
- * Component for displaying the Privacy Policy page.
- *
- * @component
- * @example
- * <PrivacyPolicy />
- */
 <template>
-    <div>
-        <!-- Privacy Policy content goes here -->
-    </div>
+    <section>
+        <h1>Privacy Policy</h1>
+        <h2>Introduction</h2>
+        <p>Given the development of new communication tools, it is necessary to pay particular attention to the protection of privacy. That is why we are committed to respecting the confidentiality of the personal information we collect.</p>
+
+        <h2>Collection of personal information</h2>
+        <p>The personal information we collect is collected through forms and through the interactivity established between you and our website. We also use, as indicated in the following section, cookies and/or logs to gather information about you.</p>
+
+        <h2>Forms and interactivity:</h2>
+        <p>Your personal information is collected through forms, namely:</p>
+        <p>We use the information collected in this way for the following purposes:</p>
+        <ul>
+            <li>- Statistics</li>
+            <li>- Contact</li>
+        </ul>
+        <p>Your information is also collected through the interactivity that may be established between you and our website, as follows:</p>
+        <ul>
+            <li>- Comments</li>
+        </ul>
+
+        <p>We use the information collected in this way for the following purposes:</p>
+        <ul>
+            <li>- Promotional offers</li>
+            <li>- Statistics</li>
+            <li>- Contact</li>
+        </ul>
+
+        <h2>Right of opposition and withdrawal</h2>
+        <p>We are committed to offering you the right to object and withdraw your personal information.</p>
+        <p>The right of opposition is understood to mean the possibility offered to Internet users to refuse that their personal information be used for certain purposes mentioned during collection.</p>
+        <p>The right of withdrawal is understood to mean the possibility offered to Internet users to request that their personal information no longer appear, for example, in a mailing list.</p>
+
+        <h3>To exercise these rights, you can:</h3>
+        <p>Postal code: 94800</p>
+        <p>Email: data-camp@gmail.com</p>
+        <p>Phone: +33732632652</p>
+
+        <h2>Right of access</h2>
+        <p>We are committed to recognizing a right of access and rectification to the persons concerned who wish to consult, modify or even delete information concerning them.</p>
+
+        <h2>Security</h2>
+        <p>The personal information we collect is kept in a secure environment. The people working for us are required to respect the confidentiality of your information.</p>
+        <p>To ensure the security of your personal information, we use the following measures:</p>
+        <p>We are committed to maintaining a high degree of confidentiality by integrating the latest technological innovations to ensure the confidentiality of your transactions. However, as no mechanism offers maximum security, there is always a risk when using the Internet to transmit personal information.</p>
+
+        <h2>Legislation</h2>
+        <p>We are committed to respecting the legislative provisions set out in:</p>
+        <p>Legislation: yes</p>
+
+        <p><strong>Privacy Policy Generator / </strong><a href="//www.politiquedeconfidentialite.ca" target="_blank"><strong>Privacy Policy</strong></a></p>
+    </section>
 </template>
 
 <script>
 export default {
     name: 'PrivacyPolicy',
-    // Component logic goes here
 }
 </script>
+
+<style scoped>
+section {
+    font-family: Arial, sans-serif;
+    margin: 0 auto;
+    max-width: 800px;
+    padding: 20px;
+    color: #333;
+}
+
+h1, h2, h3 {
+    color:  rgb(49, 141, 98);
+}
+
+h1 {
+    font-size: 2em;
+    margin-bottom: 0.5em;
+}
+
+h2 {
+    font-size: 1.5em;
+    margin-top: 1em;
+    margin-bottom: 0.5em;
+}
+
+h3 {
+    font-size: 1.2em;
+    margin-top: 1em;
+    margin-bottom: 0.5em;
+}
+
+p {
+    line-height: 1.5;
+    margin-bottom: 1em;
+    font-size: 1em;
+}
+
+ul {
+    padding-left: 20px;
+}
+
+ul li {
+    margin-bottom: 0.5em;
+    font-size: 0.9em;
+}
+
+a {
+    color:  rgb(49, 141, 98);
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+</style>
+
+
