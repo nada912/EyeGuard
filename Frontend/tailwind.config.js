8430199dfe77ed52eb7fca47a449573170281f00
Nada NADIRE <nadita8225@gmail.com>
2023-10-30 01:30:57 +0100

    new file:   Frontend/babel.config.js 	new file:   Frontend/jsconfig.json 	new file:   Frontend/package-lock.json 	new file:   Frontend/package.json 	new file:   Frontend/postcss.config.js 	new file:   Frontend/src/views/Confidentiality.vue 	new file:   Frontend/tailwind.config.js 	new file:   Frontend/vue.config.js

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+        purge: [
+                './src/**/*.html',
+                './src/**/*.vue',
+                './src/**/*.jsx',
+                './src/**/*.js',
+        ],
+        darkMode: false, // or 'media' or 'class'
+        theme: {
+            extend: {},
+        },
+        variants: {
+            extend: {},
+        },
+        plugins: [],
+}
+    
